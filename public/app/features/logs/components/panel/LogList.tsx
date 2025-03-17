ea22e2d15d8494570d9b71a4f2872fed5387a66d
Matias Chomicki <matyax@gmail.com>
2025-03-17 12:30:50 +0100

    LogListContext: sync external changes only when showControls is disabled

--- public/app/features/logs/components/panel/LogList.tsx
+++ public/app/features/logs/components/panel/LogList.tsx
@@ -112,6 +112,7 @@ export const LogList = ({
       onUnpinLine={onUnpinLine}
       pinLineButtonTooltipTitle={pinLineButtonTooltipTitle}
       pinnedLogs={pinnedLogs}
+      showControls={showControls}
       showTime={showTime}
       sortOrder={sortOrder}
       syntaxHighlighting={syntaxHighlighting}
